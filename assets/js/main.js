c461016a074b3c6de0249b4fbc5bb720b192589e
matheusmadeira <fumacammadeira@gmail.com>
2025-04-06 16:01:39 -0300

    fix: videos working at smartphones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
 // Fixa rodapé
 $(document).ready(function () {
-  if (document.body.scrollHeight && (document.body.scrollHeight > window.innerHeight)) {
+  if (
+    document.body.scrollHeight &&
+    document.body.scrollHeight > window.innerHeight
+  ) {
     $("#rodape").removeClass("fixaRodape");
     $("#row-main").css("padding-bottom", "0%");
   } else {
     $("#rodape").addClass("fixaRodape");
-    $("#row-main").css("padding-bottom", ($('#rodape').css("height")));
+    $("#row-main").css("padding-bottom", $("#rodape").css("height"));
   }
   if ($(window).scrollTop() === 0) {
     $("#row-main").css("padding-top", "0%");
-    $("#menuLateral").css("padding-bottom", ($('header').css("height")));
+    $("#menuLateral").css("padding-bottom", $("header").css("height"));
   } else {
     $("#menuLateral").css("top", 0);
     $("#sidebarButton").css("top", 0);
@@ -24,86 +27,91 @@ $(window).scroll(function teste() {
     $("#row-main").css("padding-bottom", "0%");
   } else {
     $("#rodape").addClass("fixaRodape");
-    $("#row-main").css("padding-bottom", ($('#rodape').css("height")));
+    $("#row-main").css("padding-bottom", $("#rodape").css("height"));
   }
 
   if ($(window).scrollTop() === 0) {
     $("#row-main").css("padding-top", "0%");
-    $("#menuLateral").css({ "top": "", "transition": "0.6s" }, "slow");
-    $("#sidebarButton").css({ "top": "", "transition": "0.6s;" });
-    $("#sidebarButton2").css({ "top": "", "transition": "0.6s;" });
-    $("#menuLateral").css("padding-bottom", ($('header').css("height")));
-
+    $("#menuLateral").css({ top: "", transition: "0.6s" }, "slow");
+    $("#sidebarButton").css({ top: "", transition: "0.6s;" });
+    $("#sidebarButton2").css({ top: "", transition: "0.6s;" });
+    $("#menuLateral").css("padding-bottom", $("header").css("height"));
   } else {
-    $("#menuLateral").css({ "top": "0", "transition": "0.6s;" }, "slow");
-    $("#sidebarButton").css({ "top": "0", "transition": "0.6s;" }, "slow");
-    $("#sidebarButton2").css({ "top": "0", "transition": "0.6s;" }, "slow");
+    $("#menuLateral").css({ top: "0", transition: "0.6s;" }, "slow");
+    $("#sidebarButton").css({ top: "0", transition: "0.6s;" }, "slow");
+    $("#sidebarButton2").css({ top: "0", transition: "0.6s;" }, "slow");
     $("#menuLateral").css("padding-bottom", "0");
   }
 });
 
 /* Adiciona suavidade na rolagem da ancoragem */
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    $('html,body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+    $("html,body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      500
+    );
   });
 });
 
 /* Adiciona tooltip (Abas) */
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 // SIDEBAR
 /* Abre com menu recolhido em Tablet e smartphone */
 var tam = $(window).width();
 if (tam <= 980) {
-  $('#sidebar').addClass('esconder');
+  $("#sidebar").addClass("esconder");
   $("#sidebarButton").addClass("open");
   $("#content").addClass("col-md-12");
   $("#content").removeClass("col-md-10");
 } else {
-  $('#sidebar').removeClass('esconder');
+  $("#sidebar").removeClass("esconder");
   $("#sidebarButton").removeClass("open");
   $("#content").removeClass("col-md-12");
   $("#content").addClass("col-md-10");
 }
 
 // Recolhe menu ao reduzir tela no desktop //
-$(window).on('resize', function () {
+$(window).on("resize", function () {
   if (document.body.scrollHeight >= window.innerHeight) {
     $("#rodape").removeClass("fixaRodape");
     $("#row-main").css("padding-bottom", "0%");
   } else {
     $("#rodape").addClass("fixaRodape");
-    $("#row-main").css("padding-bottom", ($('#rodape').css("height")));
+    $("#row-main").css("padding-bottom", $("#rodape").css("height"));
   }
 
   if ($(window).scrollTop() > 0) {
-    $("#row-main").css("padding-top", ($('header').css("height")));
+    $("#row-main").css("padding-top", $("header").css("height"));
   }
 
   var win = $(this);
   if (win.width() < 980) {
-
-    $('#sidebar').addClass('esconder');
+    $("#sidebar").addClass("esconder");
     $("#sidebarButton").addClass("open");
     $("#content").addClass("col-md-12");
     $("#content").removeClass("col-md-10");
-
   } else {
-    $('#sidebar').removeClass('esconder');
+    $("#sidebar").removeClass("esconder");
     $("#sidebarButton").removeClass("open");
     $("#content").removeClass("col-md-12");
     $("#content").addClass("col-md-10");
   }
-
 });
 
 /*Collapse Sidebar*/
+$(".toggle-sidebar").click(function () {
+  $("#sidebar").toggleClass("esconder"); // Adiciona/Remove a classe 'esconder' que oculta o sidebar
+  $("#content").toggleClass("col-md-12 col-md-10"); // Alterna entre ocupar toda a largura ou apenas uma parte
+  $("#sidebarButton").toggleClass("open"); // Altera a classe do botão de alternância
+  return false; // Impede o comportamento padrão do clique
+});
 $(".toggle-sidebar").click(function () {
   $("#sidebar").toggleClass("esconder");
   $("#content").toggleClass("col-md-12 col-md-10");
@@ -113,20 +121,20 @@ $(".toggle-sidebar").click(function () {
 });
 
 // CAROUSEL SWIPE
-+function ($) {
-  'use strict';
++(function ($) {
+  "use strict";
 
   if (!$.fn.carousel) {
-    throw new Error("carousel-swipe required bootstrap carousel")
+    throw new Error("carousel-swipe required bootstrap carousel");
   }
 
   // CAROUSEL CLASS DEFINITION
   // =========================
 
   var CarouselSwipe = function (element) {
-    this.$element = $(element)
-    this.carousel = this.$element.data('bs.carousel')
-    this.options = $.extend({}, CarouselSwipe.DEFAULTS, this.carousel.options)
+    this.$element = $(element);
+    this.carousel = this.$element.data("bs.carousel");
+    this.options = $.extend({}, CarouselSwipe.DEFAULTS, this.carousel.options);
     this.startX =
       this.startY =
       this.startTime =
@@ -135,156 +143,148 @@ $(".toggle-sidebar").click(function () {
       this.$items =
       this.$next =
       this.$prev =
-      this.dx = null
+      this.dx =
+        null;
 
     this.$element
-      .on('touchstart', $.proxy(this.touchstart, this))
-      .on('touchmove', $.proxy(this.touchmove, this))
-      .on('touchend', $.proxy(this.touchend, this))
-  }
+      .on("touchstart", $.proxy(this.touchstart, this))
+      .on("touchmove", $.proxy(this.touchmove, this))
+      .on("touchend", $.proxy(this.touchend, this));
+  };
 
   CarouselSwipe.DEFAULTS = {
-    swipe: 50 // percent per second
-  }
+    swipe: 50, // percent per second
+  };
 
   CarouselSwipe.prototype.touchstart = function (e) {
     if (!this.options.swipe) return;
-    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e
-    this.dx = 0
-    this.startX = touch.pageX
-    this.startY = touch.pageY
-    this.cycling = null
-    this.width = this.$element.width()
-    this.startTime = e.timeStamp
-  }
+    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e;
+    this.dx = 0;
+    this.startX = touch.pageX;
+    this.startY = touch.pageY;
+    this.cycling = null;
+    this.width = this.$element.width();
+    this.startTime = e.timeStamp;
+  };
 
   CarouselSwipe.prototype.touchmove = function (e) {
     if (!this.options.swipe) return;
-    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e
-    var dx = touch.pageX - this.startX
-    var dy = touch.pageY - this.startY
+    var touch = e.originalEvent.touches ? e.originalEvent.touches[0] : e;
+    var dx = touch.pageX - this.startX;
+    var dy = touch.pageY - this.startY;
     if (Math.abs(dx) < Math.abs(dy)) return; // vertical scroll
 
     if (this.cycling === null) {
-      this.cycling = !!this.carousel.interval
-      this.cycling && this.carousel.pause()
+      this.cycling = !!this.carousel.interval;
+      this.cycling && this.carousel.pause();
     }
-    e.preventDefault()
-    this.dx = dx / (this.width || 1) * 100
-    this.swipe(this.dx)
-  }
+    e.preventDefault();
+    this.dx = (dx / (this.width || 1)) * 100;
+    this.swipe(this.dx);
+  };
 
   CarouselSwipe.prototype.touchend = function (e) {
     if (!this.options.swipe) return;
     if (!this.$active) return; // nothing moved
     var all = $()
-      .add(this.$active).add(this.$prev).add(this.$next)
-      .carousel_transition(true)
+      .add(this.$active)
+      .add(this.$prev)
+      .add(this.$next)
+      .carousel_transition(true);
 
-    var dt = (e.timeStamp - this.startTime) / 1000
-    var speed = Math.abs(this.dx / dt) // percent-per-second
+    var dt = (e.timeStamp - this.startTime) / 1000;
+    var speed = Math.abs(this.dx / dt); // percent-per-second
     if (this.dx > 40 || (this.dx > 0 && speed > this.options.swipe)) {
-      this.carousel.prev()
+      this.carousel.prev();
     } else if (this.dx < -40 || (this.dx < 0 && speed > this.options.swipe)) {
       this.carousel.next();
     } else {
       this.$active
         .one($.support.transition.end, function () {
-          all.removeClass('prev next')
+          all.removeClass("prev next");
         })
-        .emulateTransitionEnd(this.$active.css('transition-duration').slice(0, -1) * 1000)
+        .emulateTransitionEnd(
+          this.$active.css("transition-duration").slice(0, -1) * 1000
+        );
     }
 
-    all.css('left', '')
-    this.cycling && this.carousel.cycle()
-    this.$active = null // reset the active element
-  }
+    all.css("left", "");
+    this.cycling && this.carousel.cycle();
+    this.$active = null; // reset the active element
+  };
 
   CarouselSwipe.prototype.swipe = function (percent) {
-    var $active = this.$active || this.getActive()
+    var $active = this.$active || this.getActive();
     if (percent < 0) {
-      this.$prev
-        .css('left', '')
-        .removeClass('prev')
-        .carousel_transition(true)
-      if (!this.$next.length || this.$next.hasClass('active')) return
+      this.$prev.css("left", "").removeClass("prev").carousel_transition(true);
+      if (!this.$next.length || this.$next.hasClass("active")) return;
       this.$next
         .carousel_transition(false)
-        .addClass('next')
-        .css('left', (percent + 100) + '%')
+        .addClass("next")
+        .css("left", percent + 100 + "%");
     } else {
-      this.$next
-        .css('left', '')
-        .removeClass('next')
-        .carousel_transition(true)
-      if (!this.$prev.length || this.$prev.hasClass('active')) return
+      this.$next.css("left", "").removeClass("next").carousel_transition(true);
+      if (!this.$prev.length || this.$prev.hasClass("active")) return;
       this.$prev
         .carousel_transition(false)
-        .addClass('prev')
-        .css('left', (percent - 100) + '%')
+        .addClass("prev")
+        .css("left", percent - 100 + "%");
     }
 
-    $active
-      .carousel_transition(false)
-      .css('left', percent + '%')
-  }
+    $active.carousel_transition(false).css("left", percent + "%");
+  };
 
   CarouselSwipe.prototype.getActive = function () {
-    this.$active = this.$element.find('.item.active')
-    this.$items = this.$active.parent().children()
+    this.$active = this.$element.find(".item.active");
+    this.$items = this.$active.parent().children();
 
-    this.$next = this.$active.next()
+    this.$next = this.$active.next();
     if (!this.$next.length && this.options.wrap) {
       this.$next = this.$items.first();
     }
 
-    this.$prev = this.$active.prev()
+    this.$prev = this.$active.prev();
     if (!this.$prev.length && this.options.wrap) {
       this.$prev = this.$items.last();
     }
 
     return this.$active;
-  }
+  };
 
   // CAROUSEL PLUGIN DEFINITION
   // ==========================
 
-  var old = $.fn.carousel
+  var old = $.fn.carousel;
   $.fn.carousel = function () {
     old.apply(this, arguments);
     return this.each(function () {
-      var $this = $(this)
-      var data = $this.data('bs.carousel.swipe')
-      if (!data) $this.data('bs.carousel.swipe', new CarouselSwipe(this))
-    })
-  }
+      var $this = $(this);
+      var data = $this.data("bs.carousel.swipe");
+      if (!data) $this.data("bs.carousel.swipe", new CarouselSwipe(this));
+    });
+  };
 
   $.extend($.fn.carousel, old);
 
   $.fn.carousel_transition = function (enable) {
-    enable = enable ? '' : 'none';
+    enable = enable ? "" : "none";
     return this.each(function () {
-      $(this)
-        .css('-webkit-transition', enable)
-        .css('transition', enable)
-    })
+      $(this).css("-webkit-transition", enable).css("transition", enable);
+    });
   };
 
   /* Navegação Carousel card com touch*/
   $(".carousel").carousel({
     interval: false,
-    pause: true
+    pause: true,
   });
 
-
-  $('.carousel .carousel-inner').on('dragstart', 'a', function () {
+  $(".carousel .carousel-inner").on("dragstart", "a", function () {
     return false;
   });
 
-
   /*Flip Card */
   $(".flipCard").on("click", function () {
-    $(this).toggleClass('flipped');
+    $(this).toggleClass("flipped");
   });
-
-}(jQuery);
+})(jQuery);
